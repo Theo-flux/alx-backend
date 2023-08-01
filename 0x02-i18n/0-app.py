a951b317ed70b703fb347ae14531e8e73650e7dc
Theo <ekunnusitheophilus@gmail.com>
2023-08-01 12:49:59 +0100

    basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """0. Basic Flask app
+
+    Returns:
+        _type_: _description_
+    """
+    return render_template('/0-index.html')
---
+++ 0x02-i18n/README.md
@@ -0,0 +1,3 @@
+# i8n
+i8n - internationalisation
+l10n - localisation
