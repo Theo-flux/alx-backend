06b0dc290f725dae985e13bd21e51f447804d807
Theo <ekunnusitheophilus@gmail.com>
2023-09-02 11:25:48 +0100

    feat: redis connection established

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,13 @@
+import { createClient } from 'redis';
+
+function redisCreateClient(){
+    const client = createClient();
+    client.on('connect', () => 
+        console.log('Redis client connected to the server'
+    ))
+    .on('error', (error) => 
+        console.log('Redis client not connected to the server: '+ error
+    ))
+}
+
+redisCreateClient()
